e8ea7fac18737db61ec1550d38c22eb426215fde
20sarah20 <02sarah21@gmail.com>
2025-06-29 20:00:00 -0400

    minor changes

--- proj3.js
+++ proj3.js
@@ -2,6 +2,10 @@ const use_move = (caster, target, move) => {
 
 }
 
+const moveHits = (caster, target, move) => {
+    return(move.accuracy * caster.accuracy * target.evasion > Math.floor(Math.random() * 256));
+}
+
 const damage = (caster, target, move) => {
     var level = 1;
     var critThreshold = move.status.includes("High Crit") ? Math.min(8 * Math.floor(caster.speed / 2), 255) : Math.floor(caster.speed / 2);
@@ -10,11 +14,24 @@ const damage = (caster, target, move) => {
     var attack = move.type == "special" ? caster.specialAttack : caster.attack;
     var defense = move.type == "special" ? caster.specialDefense : caster.defense;
     var damage = (((2 * level / 5 * crit + 2) * move.power * attack / defense)/ 50 + 2) * STAB * getTypeAdvantage(move.type, target.type1) * getTypeAdvantage(move.type, target.type2);
+    target.health -= damage;
 }
 // TODO
 const getTypeAdvantage = (attackerType, defenderType) =>{
     return(1);
 }
+
+/**
+ * Frozen
+ * Burned
+ * Confused
+ * Poisoned
+ * Asleep
+ * Paralyized
+ * Heal
+ * Buff Attack
+ * 
+ */
 /**
  * Move Structure
  * Target
