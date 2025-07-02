05872bc3bb15d0f39b2db302ccbed510a185cb0f
JustinC1620 <jacollier1620@gmail.com>
2025-07-02 19:25:15 -0400

    comments

--- scripts/select6.js
+++ scripts/select6.js
@@ -1,160 +1,184 @@
-const MAXLOCKED = 5;
-const BGSCALER = 0.3;
+/*
+ * File: select6.js 
+ * Author: Justin C
+ * Description: Interactive content for select6.js
+ * Requires utils.js to be in preceeding script tag 
+*/
 
-var team = new Team(null);
-var locked = new Array(6).fill(false);
-var numLocked = 0;
+// Global Constants
+const MAXLOCKED = 5;                    // Max number of locked pokemon
+const BGSCALER = 0.3;                   // Scales background music down. This song is loud!
+
+// Global Variables
+var team = new Team(null);              // The current team on screen
+var locked = new Array(6).fill(false);  // Array of which pokemon are locked
+var numLocked = 0;                      // Number of locked pokemon
 
 $(function () {
-    team = JSON.parse(document.getElementById("teamJSON").innerHTML);
-    volume = JSON.parse(document.getElementById("volumeJSON").innerHTML);
-    muted = JSON.parse(document.getElementById("mutedJSON").innerHTML);
+    // Gets Data Passed From Previous Page
+    team = JSON.parse(document.getElementById("teamJSON").innerHTML);      // Gets team data
+    volume = JSON.parse(document.getElementById("volumeJSON").innerHTML);  // Gets volume setting
+    muted = JSON.parse(document.getElementById("mutedJSON").innerHTML);    // Gets muted setting
 
+    // Gets Audio Tags And Sets Volume
     music = document.getElementById('bgMusic');
     music.volume = volume * VSCALER * BGSCALER;
     music.muted = muted;
+
     click = document.getElementById('btnClk');
     click.volume = volume * VSCALER;
     click.muted = muted;
 
-    if (!muted) {
+    if (!muted) {  // Starts Music
         music.play();
     }
 
+    // Event For Button Click Sound
     document.querySelectorAll('button').forEach(button => {
         button.addEventListener('click', () => {
-            playSound(click, muted);
+            playSound(click, muted);  // Plays sound if not muted
         });
     });
 
+    // Event For Confirm Button
     document.getElementById("confirm").addEventListener("click", function () {
-        playSound(click, muted);
+        // Create Form
         let form = document.createElement("form");
         form.style.visibility = "hidden";
         form.method = "POST";
         form.action = "./select_moves.php";
 
+        // Adds Team JSON String To $_POST
         let ipt = document.createElement("input");
         ipt.name = "team";
         ipt.value = JSON.stringify(team);
-        form.appendChild(ipt);
-        document.body.appendChild(form);
+        form.appendChild(ipt);            
 
+        // Adds Volume To $_POST
         ipt = document.createElement("input");
         ipt.name = "volume";
         ipt.value = JSON.stringify(volume);
         form.appendChild(ipt);
-        document.body.appendChild(form);
 
+        // Adds Mute to $_POST
         ipt = document.createElement("input");
         ipt.name = "muted";
         ipt.value = muted;
         form.appendChild(ipt);
-        document.body.appendChild(form);
 
-        form.submit();
+        // Append Form And Submit
+        document.body.appendChild(form);
+        form.submit();  // Redirects to select_moves.php
     });
 
+    // Event For Back Button
     document.getElementById("back").addEventListener("click", function () {
-        playSound(click, muted);
+        // Create Form
         let form = document.createElement("form");
         form.style.visibility = "hidden";
         form.method = "POST";
         form.action = "./proj3.php";
-        document.body.appendChild(form);
 
+        // Adds Volume To $_POST
         ipt = document.createElement("input");
         ipt.name = "volume";
         ipt.value = JSON.stringify(volume);
         form.appendChild(ipt);
-        document.body.appendChild(form);
 
+        // Adds Mute to $_POST
         ipt = document.createElement("input");
         ipt.name = "muted";
         ipt.value = JSON.stringify(muted);
         form.appendChild(ipt);
-        document.body.appendChild(form);
 
-        form.submit();
+        // Append Form And Submit
+        document.body.appendChild(form);
+        form.submit();  // Redirects to proj3.php
     });
 
+    // Event For Reroll Button
     document.getElementById("reroll").addEventListener("click", function () {
-        let ids = [];
-        for (let i = 0; i < TEAMSIZE; i++) {
-            if (locked[i]) {
+        let ids = [];                                       // Array of urrent pokemon ids
+        for (let i = 0; i < TEAMSIZE; i++) {                // Iterate over team
+            if (locked[i]) {                                // Appends if locked
                 ids.push(parseInt(team.pkm[i].id));
             }
         }
-        let newIds = [];
-        for (let i = 0; i < TEAMSIZE - numLocked; i++) {
-            let n;
+        let newIds = [];                                    // Array of new pokemon ids
+        for (let i = 0; i < TEAMSIZE - numLocked; i++) {    // For number of pokemon not locked
+            let n;                                          // New id
             do {
-                n = genId()
+                n = genId()                                 // Generate valid id
             }
-            while (newIds.includes(n) || ids.includes(n));
+            while (newIds.includes(n) || ids.includes(n));  // Ensure no duplicate pokemon 
             newIds.push(n);
         }
-        console.log(ids);
-        console.log(newIds);
-        newIds = newIds.join(",");
+        newIds = newIds.join(",");                          // Convert newIds to CSV for get request
+
+        // Gets new pokemon from server. Handled by setPokemon()
         $.get("./scripts/get_pokemon.php", { "ids": newIds }, setPokemon, "json");
     });
 
+    // Event For Pokemon Entries
     document.querySelectorAll(".entry").forEach(e => {
         e.addEventListener("click", function () {
-            if (e.classList.contains("locked")) {
+            if (e.classList.contains("locked")) {  // Unlocks pokemon 
                 e.classList.remove("locked");
                 locked[e.id[e.id.length - 1]] = false;
-                numLocked--;
+                numLocked--;    
             }
-            else if (numLocked < MAXLOCKED) {
+            else if (numLocked < MAXLOCKED) {      // Locks pokemon
                 e.classList.add("locked");
                 locked[e.id[e.id.length - 1]] = true;
                 numLocked++;
             }
-            else if (numLocked === MAXLOCKED) {
+            else if (numLocked === MAXLOCKED) {    // Prevents locking all pokemon
                 window.alert("Only 5 Pokemon can be locked at a time!");
             }
         });
     });
 
+    // Event For Sound Toggle Button
     document.getElementById("sound").addEventListener("click", function () {
-        if (muted) {
+        if (muted) {  // Unmutes and starts music
             music.muted = false;
             click.muted = false;
             muted = false;
             music.currentTime = 0;
             music.play();
         }
-        else {
+        else {        // Mutes 
             music.muted = true;
             click.muted = true;
             muted = true;
         }
     });
 
+    // Event For Volume Up Button
     document.getElementById("up").addEventListener("click", function () {
-        if (!muted) {
+        if (!muted) {             // Only updates if not muted
             volume = volume + 5;
-
-            if (volume > MAXV) {
+            if (volume > MAXV) {  // Clamps volume and alerts user
+                volume = MAXV;
                 window.alert("Max Volume");
             }
-            else {
+            else {                // Updates music and click volume
                 music.volume = volume * VSCALER * BGSCALER;
                 click.volume = volume * VSCALER;
             }
         }
     });
 
+    // Event For Volume Down Button
     document.getElementById("down").addEventListener("click", function () {
-        if (!muted) {
+        if (!muted) {             // Only updates if not muted
             volume = volume - 5;
 
-            if (volume < MINV) {
+            if (volume < MINV) {  // Clamps volume and alerts user
+                volume = MINV;
                 window.alert("Minimum Volume");
             }
-            else {
+            else {                // Updates music and click volume
                 music.volume = volume * VSCALER * BGSCALER;
                 click.volume = volume * VSCALER;
             }
@@ -162,40 +186,46 @@ $(function () {
     });
 });
 
+// setPokemon()
+// Handles data returned from get_pokemon.php
 function setPokemon(newPkm) {
-    let curNew = 0;
-    for (let i = 0; i < TEAMSIZE; i++) {
+    let curNew = 0;                                                         // Index for currently replaced pokemon
+    for (let i = 0; i < TEAMSIZE; i++) {                                    // Iterates over team
         if (!locked[i]) {
-            team.pkm[i] = newPkm[curNew];
+            team.pkm[i] = newPkm[curNew];                                   // Updates pokemon in team
+
+            // Updates Stats On Screen
             pkm = team.pkm[i];
-            let elm = document.getElementById("pkm" + i).children[1];
-            for (let j = 0; j < NUMSTATS; j++) {
+            let elm = document.getElementById("pkm" + i).children[1];       // Selects pokemon entry righthand side
+            for (let j = 0; j < NUMSTATS; j++) {                            // Updates stats on screen
                 elm.children[j].innerHTML = Object.values(pkm.attr)[j];
             }
-            elm = document.getElementById("pkm" + i).children[0].children;
-            for (let j = 0; j < elm.length; j++) {
+            elm = document.getElementById("pkm" + i).children[0].children;  // Selects pokemon entry lefthand side
+            for (let j = 0; j < elm.length; j++) {                          // Iterates over entries
                 switch (j) {
-                    case 0:
+                    case 0:                                                 // Updates pokemon id and image on screen
                         elm[j].name = pkm.id;
                         elm[j].src = pkm.img;
                         break;
-                    case 1:
+                    case 1:                                                 // Updates pokemon name on screen
                         elm[j].innerHTML = pkm.name;
                         break;
-                    case 2:
-                        elm[j].innerHTML = "";
+                    case 2:                                                 // Updates pokemon type on screen
+                        elm[j].innerHTML = "";                              // Clears images
                         for (let k = 0; k < pkm.types.length; k++) {
-                            if (!pkm.types[k]) {
+                            if (!pkm.types[k]) {                            // If pokemon has only 1 type
                                 break;
                             }
+                            // Create New Type Image Element
                             let img = document.createElement("img");
                             img.classList.add("type-icon");
                             img.src = "./dataFiles/typeIcons/" + pkm.types[k].toLowerCase() + ".png";
                             img.alt = pkm.types[k];
-                            elm[j].appendChild(img);
+
+                            elm[j].appendChild(img);                        // Apends image to entry
                         }
                         break;
-                    default:
+                    default:                                                // Log unexpected case
                         console.log("Switch Statement Error! Unmatched Case " + j);
 
                 }
--- select6.php
+++ select6.php
@@ -1,10 +1,16 @@
+<!-- 
+* File: select6.js 
+ * Author: Justin C & Kevin K 
+ * Description: Page where user selects their pokemon team
+-->
 <?php
 require "scripts/utils.php";
 
-if (array_key_exists("team", $_POST)){
+if (array_key_exists("team", $_POST)){      // If redirected from select_moves.php
+
     $team = json_decode($_POST["team"]);
 }
-else {
+else {                                      // If redirected from proj3.php
     // Generate 6 random IDs between 1 and 151 excluding 132 (Ditto has no moves)
     $randIds = range(1, 151);
     unset($randIds[131]);
@@ -24,8 +30,8 @@ else {
     }
 }
 
-$volume = $_POST["volume"];
-$muted = $_POST["muted"];
+$volume = $_POST["volume"]; // Get volume setting 
+$muted = $_POST["muted"];   // Get mute setting 
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +41,7 @@ $muted = $_POST["muted"];
     <title>Choose your Pokémon</title>
     <link rel="stylesheet" href="styles/shared.css">
     <link rel="stylesheet" href="styles/select6.css">
-    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">  <!-- Link to font -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="./scripts/utils.js"></script>
     <script src="./scripts/select6.js"></script>
@@ -47,62 +53,61 @@ $muted = $_POST["muted"];
 <audio id="btnClk" muted hidden>
     <source src="dataFiles/audio/sfx/Pokemon_(A_Button).mp3">
 </audio>
-<div class="center">           <!-- Center block of content -->
-<div class="container">        <!-- Pokedex Grey Border -->
-    <h1>Choose Your Team</h1> 
-    <div class="center">       <!-- Center block of content -->
-    <div class="screen">       <!-- Pokedex Screen -->
-    <!-- Screen Content -->
-            <div class="bor header-row">
-                <span class="left">Pokemon Types</span>
-                <span class="right">
-                    <?php foreach ($team->pkm[0]->attr as $key => $stat): ?>
-                        <?php if ($key != "legendary"): ?>
-                            <span class="stat <?= $key ?>-stat"><?= ucfirst($key) ?></span>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </span>
-            </div>
-            <?php foreach ($team->pkm as $i => $pkm): ?>
-                <button type="button" class="entry" id="pkm<?= $i ?>">
-                    <div class="left">
-                        <img src="<?= $pkm->img ?>" name="<?= $pkm->id ?>">
-                        <span class="name"><?= $pkm->name ?></span>
-                        <span class="types">
-                            <?php foreach ($pkm->types as $type): ?>
-                                <?php if ($type): ?>
-                                    <img class="type-icon"
-                                            src="<?= FPATH . TPATH . strtolower($type) ?>.png"
-                                            alt="<?= $type ?>">
-                                <?php endif; ?>
-                            <?php endforeach; ?>
-                        </span>
-                    </div>
-                    <div class="right">
-                        <?php foreach ($pkm->attr as $j => $stat): ?>
-                            <?php if ($j != "legendary"): ?>
-                                <span class="stat <?= $j ?>-stat" name="stat<?= $j ?>"><?= $stat ?></span>
+<div class="center">                                              <!-- Center Block Of Content -->
+    <div class="container">                                           <!-- Pokedex Grey Border -->
+        <h1>Choose Your Team</h1>                                         <!-- Page Title -->
+        <div class="center">                                              <!-- Center Block Of Content -->
+            <div class="screen">                                              <!-- Pokedex Screen -->
+                <div class="bor header-row">                                      <!-- Page Header -->
+                    <span class="left">Pokemon Types</span>                           <!-- Pokemon Name and Type Header -->
+                    <span class="right">                                              <!-- Pokemon Stats Header -->
+                        <?php foreach ($team->pkm[0]->attr as $key => $stat): ?>          <!-- Pokemon Stat Header -->
+                            <?php if ($key != "legendary"): ?>
+                                <span class="stat <?= $key ?>-stat"><?= ucfirst($key) ?></span>
                             <?php endif; ?>
                         <?php endforeach; ?>
-                    </div>
-                </button>
-            <?php endforeach; ?>
+                    </span>
+                </div>
+                <?php foreach ($team->pkm as $i => $pkm): ?>                      <!-- Pokemon Entries -->
+                    <button type="button" class="entry" id="pkm<?= $i ?>">            <!-- Pokemon Entry -->
+                        <div class="left">
+                            <img src="<?= $pkm->img ?>" name="<?= $pkm->id ?>">           <!-- Pokemon Image Entry -->
+                            <span class="name"><?= $pkm->name ?></span>                   <!-- Pokemon Name Entry -->
+                            <span class="types">                                          <!-- Pokemon Types Entry -->
+                                <?php foreach ($pkm->types as $type): ?>                      <!-- Pokemon Type image Entry -->
+                                    <?php if ($type): ?>
+                                        <img class="type-icon"
+                                                src="<?= FPATH . TPATH . strtolower($type) ?>.png"
+                                                alt="<?= $type ?>">
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </span>
+                        </div>
+                        <div class="right">                                               <!-- Pokemon Stats Entry -->
+                            <?php foreach ($pkm->attr as $j => $stat): ?>                     <!-- Pokemon Stat Entry -->
+                                <?php if ($j != "legendary"): ?>
+                                    <span class="stat <?= $j ?>-stat" name="stat<?= $j ?>"><?= $stat ?></span>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </div>
+                    </button>
+                <?php endforeach; ?>
+            </div>
+        </div>                                                                            
+        <div class="center">                                                              <!-- Center Block Of Content -->
+            <div class="menu">                                                                <!-- Menu Buttons -->
+                <button type="button" class="back" id="back">Back</button>                        <!-- Back Button -->
+                <button id="sound">Toggle Sound</button>                                          <!-- Toggle Sound Button -->
+                <button id="up">Volume Up</button>                                                <!-- Volume Up Button -->
+                <button id="down">Volume Down</button>                                            <!-- Volume Down Button -->
+                <button type="button" class="reroll" id="reroll">Reroll Pokemon</button>          <!-- Reroll Button -->
+                <button type="button" class="confirm" id="confirm">Confirm Team</button>          <!-- Confirm Button -->
+            </div>
+        </div> 
     </div>
-    </div>
-    <div class="center"> 
-    <div class="menu">
-        <button type="button" class="back" id="back">Back</button>
-        <button id="sound">Toggle Sound</button>
-        <button id="up">Volume Up</button>
-        <button id="down">Volume Down</button>
-        <button type="button" class="reroll" id="reroll">Reroll Pokemon</button>
-        <button type="button" class="confirm" id="confirm">Confirm Team</button>
-    </div>
-    </div> 
 </div>
-</div>
-<div id="teamJSON" hidden><?= json_encode($team) ?></div>
-<div id="volumeJSON" hidden><?= $volume ?></div>
-<div id="mutedJSON" hidden><?= $muted ?></div>
+<div id="teamJSON" hidden><?= json_encode($team) ?></div>         <!-- Team Data For JS -->
+<div id="volumeJSON" hidden><?= $volume ?></div>                  <!-- Volume Data For JS -->
+<div id="mutedJSON" hidden><?= $muted ?></div>                    <!-- Mute Data For JS -->            
 </body>
 </html>
